81e2d8e8e0a03fa16972301634819a25b6e98389
Your Name <william.fleckenstein@gmail.com>
2019-12-13 04:18:10 -0600

    added keywords and a description to home component.

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,11 @@ import React, { useEffect, useState } from "react";
 import Layout from "./layout/layout";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./pages/home/home";
-import Practice from "./practice";
 
 function App() {
   return (
     <Layout>
       <Switch>
-        <Route exact path="/practice" component={Practice}></Route>
         <Route exact path="/" component={Home}></Route>
         <Redirect to="/" />
       </Switch>
--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -2,10 +2,22 @@ import React from "react";
 import { BackgroundWrapper } from "../../layout/elements/index";
 import HomeContainer from "../../components/HomeContainer/HomeContainer";
 import MovingBg from "../../components/MovingBg/MovingBg";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   return (
     <BackgroundWrapper bgBlack>
+      <Helmet>
+        <title>Rest PeeP</title>
+        <meta
+          name="description"
+          content="Discover random quotes from lil peep"
+        />
+        <meta
+          name="keywords"
+          content="lil peep, lil PEEP, GBC, goth bio clique, quote generator, lil peep quotes, quotes, lil peep tattos, hellboy, cry baby, lil peeps tweets"
+        />
+      </Helmet>
       <MovingBg />
       <HomeContainer />
     </BackgroundWrapper>
