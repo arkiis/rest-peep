9c0cd6cea69c7f7e6e0876b2a364079bb72c8891
Your Name <william.fleckenstein@gmail.com>
2019-12-13 03:57:18 -0600

    increased z-index on refresh button

--- client/src/components/RefreshQuote/RefreshQuote.styles.js
+++ client/src/components/RefreshQuote/RefreshQuote.styles.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   margin-top: 2rem;
   display: flex;
+  position: relative;
+  z-index: 2;
   flex-direction: column;
   align-items: center;
   opacity: 1;
